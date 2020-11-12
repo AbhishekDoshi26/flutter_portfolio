74046492ff1e4bab804392899d66118ba24f5f75
Abhishek Doshi <adoshi26.ad@gmail.com>
2020-11-12 20:29:42 +0530

    Added About page

---
+++ lib/about.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
+
+class About extends StatelessWidget {
+  List<String> welcomeImages = [
+    "assets/images/flutter.png",
+    "assets/images/community.png",
+    "assets/images/leader.png",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    CardController controller; //Use this to trigger swap.
+    return TinderSwapCard(
+      swipeUp: true,
+      swipeDown: true,
+      orientation: AmassOrientation.BOTTOM,
+      totalNum: welcomeImages.length,
+      stackNum: 3,
+      swipeEdge: 4.0,
+      maxWidth: MediaQuery.of(context).size.width * 0.9,
+      maxHeight: MediaQuery.of(context).size.width * 1.2,
+      minWidth: MediaQuery.of(context).size.width * 0.89,
+      minHeight: MediaQuery.of(context).size.width * 1.19,
+      cardBuilder: (context, index) => Card(
+        elevation: 20.0,
+        shadowColor: Colors.white60,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20.0),
+          ),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+              child: Image.asset('${welcomeImages[index]}'),
+            ),
+          ],
+        ),
+      ),
+      cardController: controller = CardController(),
+      swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+        /// Get swiping card's alignment
+        if (align.x < 0) {
+          //Card is LEFT swiping
+        } else if (align.x > 0) {
+          //Card is RIGHT swiping
+        }
+      },
+      swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
+        /// Get orientation & index of swiped card!
+      },
+    );
+  }
+}
